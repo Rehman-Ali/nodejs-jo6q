49b628f34e8043a717b293de1807ffc85e0cc133
Rehman Ali <softdix@gmail.com>
2023-05-17 20:34:58 +0500

    updated

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const http = require('http');
 const PORT = 3000;
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
+app.use(cors());
+
+
+// configure the app to use bodyParser()
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
+
+app.use("/", require("./routes/index"));
+
+
+
+
+const server = http.createServer();
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
